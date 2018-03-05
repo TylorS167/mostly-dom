e9f9815d5651dc804ec4540646a729d47e30241e
Frederik Krautwald <fkrautwald@gmail.com>
2018-03-06 00:13:51 +0100

    Fix unsatisfied constraint in VNodeEventTypes.ts
    Fix `Type 'EventMap[K]' does not satisfy the constraint 'Event'.`

--- src/types/VNodeEventTypes.ts
+++ src/types/VNodeEventTypes.ts
@@ -5,7 +5,7 @@ export type VNodeEvent<T, Ev extends Event> = Ev & { target: T }
 export type EventHandler<T extends Element, Ev extends Event> =
   (event: VNodeEvent<T, Ev>, vNode: ElementVNode<T>) => any
 
-export type VNodeEvents<T extends Element, EventMap extends object> = {
+export type VNodeEvents<T extends Element, EventMap extends { [K in keyof EventMap]: Event }> = {
   readonly [K in keyof EventMap]?: EventHandler<T, EventMap[K]>
 }
 
