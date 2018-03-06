e505cb3ef8fba5c27e87a5562b5abd48b4915ab9
Tylor Steinberger <tylor@tryfynd.com>
2018-03-06 08:28:21 -0500

    feat: remove dependencies

--- src/hyperscript/querySelector.ts
+++ src/hyperscript/querySelector.ts
@@ -1,30 +1,33 @@
-import { CurriedFunction2, curry2 } from '@most/prelude'
-
 import { VNode } from '../types'
 import { hasCssSelector } from './hasCssSelector'
 
-export const querySelector: QuerySelector = curry2(
-  function(cssSelector: string, vNode: VNode): VNode | null {
-    const scope = vNode.scope
+export const querySelector: QuerySelector = ((cssSelector: string, vNode?: VNode) => vNode
+  ? _querySelector(cssSelector, vNode)
+  : (_vNode: VNode) => _querySelector(cssSelector, _vNode)) as QuerySelector
 
-    const children: Array<VNode> = [ vNode ]
+function _querySelector(cssSelector: string, vNode: VNode): VNode | null {
+  const scope = vNode.scope
 
-    while (children.length > 0) {
-      const child = children.shift() as VNode
+  const children: Array<VNode> = [ vNode ]
 
-      if (child.scope !== scope)
-        continue
+  while (children.length > 0) {
+    const child = children.shift() as VNode
 
-      if (hasCssSelector(cssSelector, child))
-        return child
+    if (child.scope !== scope)
+      continue
 
-      if (!child.children) continue
+    if (hasCssSelector(cssSelector, child))
+      return child
 
-      children.push(...child.children)
-    }
+    if (!child.children) continue
 
-    return null
-  },
-)
+    children.push(...child.children)
+  }
 
-export interface QuerySelector extends CurriedFunction2<string, VNode, VNode | null> {}
+  return null
+}
+
+export interface QuerySelector {
+  (cssSelector: string, vNode: VNode): VNode | null
+  (cssSelector: string): (vNode: VNode) => VNode | null
+}
